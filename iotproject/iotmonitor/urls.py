785fa29af4b2c7fa1fd96556a17559398e67760e
otavio-maciel <56772395+otavio-maciel@users.noreply.github.com>
2020-07-20 21:12:40 -0300

    Add files via upload

--- iotproject/iotmonitor/urls.py
+++ iotproject/iotmonitor/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 from django.conf import settings
-from django.template.response import TemplateResponse
+from django.template.response import TemplateResponse 
 
 urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('description/<int:pk>/', views.description, name='description'),
+    path('new/', views.new_reading, name='new_reading'),
+    path('login/', views.do_login, name='login'),
+    path('logout/', views.do_logout, name='logout'),
 ]
 
 #if settings.DEBUG:
--- iotproject/iotmonitor/views.py
+++ iotproject/iotmonitor/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Thing, Sensor, Reading, TypeOfThing
+from .forms import Leitura
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def post_list(request):
+	types = TypeOfThing.objects.all().order_by('name')
+	things = Thing.objects.all().order_by('name')
+	return render(request, 'iotmonitor/post_list.html', {'things': things, 'types': types})
+
+
+def description(request, pk):
+	thing = Thing.objects.get(pk=pk)
+	sensors = thing.sensor_set.all()
+	return render(request, 'iotmonitor/description.html', {'thing': thing, 'sensors': sensors}) #Contexto do Sensor, como fazer?
+
+@csrf_exempt
+def new_reading(request):
+		print('request received')
+		print(request.POST.get('device'))
+		print(request.POST.get('password'))
+		print(request.POST.get('id'))
+		print(request.POST.get('sensor_read'))
+		user = authenticate(username=request.POST.get('device'), password=request.POST.get('password'))
+		if user is not None:
+			Nova_Leitura = Sensor.objects.get(pk=request.POST.get('id')) #pk = ID do sensor ---> Sensor de PH = 2.
+			Nova_Leitura.reading_set.create(sensor_read=request.POST.get('sensor_read')) # sensor_read = leitura do sensor
+			Nova_Leitura.save()
+			return HttpResponse('Usuario Logado')
+		else:
+			return HttpResponse('Usuario Não Logado')
+
+def do_login(request):
+	if request.method == 'POST':
+		user = authenticate(username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+			return redirect('/new')
+	return render(request, 'iotmonitor/login.html')
+
+def do_logout(request):
+	logout(request)
+	return redirect('/login')
