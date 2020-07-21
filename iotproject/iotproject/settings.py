127973fc2355c300a2329a0e26e6130b9602b912
João Pimentel <joaozinho@gmail.com>
2020-07-21 17:35:01 -0300

    Revert "handle empty tables and sort list of things alphabetically"
    This reverts commit c2a48a5f

--- iotproject/iotproject/settings.py
+++ iotproject/iotproject/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'hsg0l6agm#8=b*qy5t00e6hk7_elj7$x1vngldiv5&wvy_w1(g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['localhost', '192.168.1.4']
 ALLOWED_HOSTS = ['localhost']
 
 
