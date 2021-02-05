426c2cc277457c239ee4198636e3f42e552fb0b9
João <joaozinho@gmail.com>
2021-02-05 09:35:38 -0300

    open browser on startup

--- iotproject/iotmonitor/apps.py
+++ iotproject/iotmonitor/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+import webbrowser
+import os
 
 
 class IotmonitorConfig(AppConfig):
     name = 'iotmonitor'
+
+    def ready(self):
+        # Check if it's running in the startup or not, to prevent running twice on startup and running again on reload
+        if os.environ.get('RUN_MAIN') != 'true':
+            with open('ip.txt', 'r') as file:
+                ip = file.readline()
+                print('Opening default browser...')
+                webbrowser.open("http://{}:8000/iotmonitor".format(ip))
