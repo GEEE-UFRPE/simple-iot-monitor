c682606f0a2563a7233117f33334eb4e70128208
João <joaozinho@gmail.com>
2021-02-10 01:40:54 -0300

    uniform plural routes

--- iotproject/iotmonitor/urls.py
+++ iotproject/iotmonitor/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.thing_list, name='thing_list'),
-    path('thing/<int:pk>/', views.thing_detail, name='thing_detail'),
+    path('things/<int:pk>/', views.thing_detail, name='thing_detail'),
     path('readings', views.new_reading, name='new_reading'),
 ]
