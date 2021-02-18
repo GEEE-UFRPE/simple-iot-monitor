0eb992f46192b71447fcb1d2416b6e5118cdee82
João <joaozinho@gmail.com>
2021-02-18 12:02:14 -0300

    make it more similar to the C example

--- client/ESP/ESP_micropython/Esp_micropython.py
+++ client/ESP/ESP_micropython/Esp_micropython.py
@@ -2,44 +2,36 @@ import network
 import urequests
 import ujson
 
-#Wifi configuration
-ssid = '' #Your wifi name.
-wifi_password = ''
-your_ip = '' # Example: '192.168.0.1'
+# Wifi configuration
+SSID = 'WIFI_NAME' # Name of the wifi network to connect
+NETWORK_PASSWORD = 'WIFI_PASSWORD' # Password of the wifi network
+IP_ADDRESS = 'SERVER_IP_ADRESS' # IP address of the server. Example: '192.168.1.1'
 
-#Server configuration
-url ="http://"+your_ip+":8000/iotmonitor/readings"
+# Sensor authentication
+SENSOR_NAME = 'sensor1' # Sensor username
+SENSOR_PASSWORD = 'badpass1' # Sensor password
 
-#Admin configuration
-sensor_name = 'sensor1' #The name of superuser created in the tutorial.
-sensor_password = 'senharuim'
 
-#Sensor Configuration
-value = ''   #This variable will be sent. you can move it if necessary.
-
+# Connecting to wifi
+url ="http://"+IP_ADDRESS+":8000/iotmonitor/readings"
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-if not wlan.isconnected():
+while not wlan.isconnected():
     print('connecting to network...')
-    wlan.connect(ssid, wifi_password)
-    while not wlan.isconnected():
-        pass
+    wlan.connect(SSID, NETWORK_PASSWORD)
 print('network config:', wlan.ifconfig())
 
-payload = "sensor="+sensor_name+"&password="+sensor_password+"&value="+value
-
-headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Cookie': 'csrftoken=eTG4ssEqH1itVNKLNCNhnjB4Fxvpo52teSA9QSx6zeag2oQDbXtM9oxjeSDLfT0k'
-}
-
-response = urequests.post(url, headers = headers , data = payload)
-
-print(response.text.encode('utf8'))
+def sendValue(sensor_username, sensor_pass, value):
+    # Send data to the server
+    parameters = "sensor="+sensor_username+"&password="+sensor_pass+"&value="+value
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = urequests.post(url, headers = headers, data = parameters)
+    print(response.text.encode('utf8'))
 
 
-
-
-
-
-
+# Actually send the data to the server. This example sends the value 5.2.
+# If you have multiple sensors on this device, you can use the sendValue function multiple times,
+# with different sensor names and passwords.
+sendValue(SENSOR_NAME, SENSOR_PASSWORD, str(5.2))
